3dc465afd4cac2d26917ab5a4b4be9068c2ba23f
Djastas <timebrocop@gmail.com>
2024-12-23 16:51:13 +0500

    add Shortcut for render pass

---
+++ Assets/Corp_Kaktus/Scripts/Trash/RenderPassActiveController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Corp_Kaktus.Scripts.Trash
+{
+    
+    public class RenderPassActiveController : MonoBehaviour
+    {
+        [SerializeField] private bool active;
+        [SerializeField] private List<GameObject> controlObjects;
+
+        private void Switch()
+        {
+            active = !active;
+            UpdateState();
+        }
+
+        private void UpdateState()
+        {
+            foreach (var controlObject in controlObjects)
+            {
+                controlObject.SetActive(active);
+            }
+        }
+
+        [MenuItem("Tools/RenderPass/SwitchActive %w")]
+        private static void SwitchStatic()
+        {
+           FindObjectOfType<RenderPassActiveController>().Switch();
+        }
+    }
+}
